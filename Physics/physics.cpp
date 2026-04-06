c40c90c4402e7f236c6b1493cb12f591b4515d10
Huaiyu <mahuaiyu2@gmail.com>
2026-04-05 17:35:05 -0700

    Updated for equation clarification

--- Physics/physics.cpp
+++ Physics/physics.cpp
@@ -10,11 +10,15 @@ void PhysicsEngine::computeForces(std::vector<Body>& bodies)
     {
         for(int j = i + 1; j < bodies.size(); j++)
         {
+
+            //Vector direction (3d vector)
             glm::vec3 dir = bodies[j].position - bodies[i].position;
             float dist = glm::length(dir) + 0.01f;
 
+            //F = G*M(1)M(2)/r^2
             float force = G * bodies[i].mass * bodies[j].mass / (dist * dist);
             
+            //Force Vector = directional vector * force 
             glm::vec3 acc = glm::normalize(dir) * force;
 
             bodies[i].acceleration += acc / bodies[i].mass;
--- Utility/Integrator.h
+++ Utility/Integrator.h
@@ -3,6 +3,7 @@
 
 inline void verlet(Body& b, float dt)
 {
+    //X(f) = X(i) + V(i) * t + 1/2*a*t^2
     glm::vec3 newPos = b.position + b.velocity * dt + 0.5f * b.acceleration * dt * dt;
     glm::vec3 newVel = b.velocity + b.acceleration * dt;
 
